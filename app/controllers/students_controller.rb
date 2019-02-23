5135113bf778d199fd20eef39ae9f7ddda0863ac
Sharmaine Requerme <srqrme@gmail.com>
2019-02-23 20:21:52 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-  Student.create(first_name: params[:student][:first_name], description: params[:post][:description])
-  redirect_to posts_path
+  Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  redirect_to new_student_path
 end
 end
